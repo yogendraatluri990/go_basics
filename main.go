1d8440699b82847bbb4a0c1a609a7cf490e5969c
yogendraatluri990 <yogendraatlurigit@gmail.com>
2026-07-17 18:40:11 -0400

    changing how channels work

--- main.go
+++ main.go
@@ -33,12 +33,11 @@ func main() {
 	signUpEvents := <-tasks
 	fmt.Printf("TaskEvents: -> Task.Title: %s  and Task.Task: %s\n", signUpEvents.Title, signUpEvents.Task)
 	if signUpEvents.Task == basicsApp.TaskDone && signUpEvents.Title == basicsApp.TaskSignupComplete {
+		go basicsApp.HandleLogInfo(user, tasks)
 		tasks <- basicsApp.TaskEvent{
 			Title: basicsApp.TaskUser,
 			Task:  basicsApp.TaskPending,
 		}
-		go basicsApp.HandleLogInfo(user, tasks)
-
 	}
 
 	userProfilTask := <-tasks
